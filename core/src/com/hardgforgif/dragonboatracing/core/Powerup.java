98c3d5645b36f07a869c81ee81bdfb4326cdd74e
Josh <js2961@york.ac.uk>
2021-02-01 17:26:07 +0000

    Switched to original method of creating body

--- core/src/com/hardgforgif/dragonboatracing/core/Powerup.java
+++ core/src/com/hardgforgif/dragonboatracing/core/Powerup.java
@@ -11,44 +11,61 @@ import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 import com.hardgforgif.dragonboatracing.BodyEditorLoader;
 import com.hardgforgif.dragonboatracing.GameData;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 
 public class Powerup extends Obstacle {
 
-    public Sprite obstacleSprite;
-    private Texture obstacleTexture;
-    public Body obstacleBody;
     private int type;
     public static int typesNo = 3;
 
-    Powerup(String textureName) {
+    Powerup(int type) {
 
-        super(textureName);
-        this.randomiseType();
-
-    }
-
-    Powerup(String textureName, int type) {
-
-        super(textureName);
+        super("powerup" + String.valueOf(type) + ".png");
         this.type = type;
 
     }
 
-    public void createObstacleBody(World world, float posX, float posY) {
+    public void createObstacleBody(World world, int posX, int posY, int width, int height) {
+
+        this.obstacleBody = createBox(posX, posY, width, height, false, world);
+        this.obstacleBody.setLinearVelocity(0, -0.2f);
+
+        obstacleBody.setUserData(this);
 
     }
 
-    public void randomiseType() {
+    private Body createBox(int x, int y, int width, int height, boolean isStatic, World world) {
 
-        Random random = new Random();
-        this.type = random.nextInt(Powerup.typesNo);
+        // Creates a body and body definition (properties for a body)
+        Body body;
+        BodyDef def = new BodyDef();
 
-    }
+        // Allows the programmer to define whether a body is static or not
+        if (isStatic) {
+            def.type = BodyDef.BodyType.StaticBody;
+        } else {
+            def.type = BodyDef.BodyType.DynamicBody;
+        }
 
-    public void setType(int t) {
+        // Sets the position of the body according to the scale of the game
+        def.position.set(x / GameData.TILES_TO_METERS, y / GameData.TILES_TO_METERS);
 
-        this.type = t;
+        // Fixes the rotation of the object
+        def.fixedRotation = true;
 
+        // Adds the body to the game world
+        body = world.createBody(def);
+
+        // Sets the shape of the body to be a box polygon
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(width / 2f / GameData.TILES_TO_METERS, height / 2f / GameData.TILES_TO_METERS);
+
+        // Fixes the box to the body
+        body.createFixture(shape, 1.0f);
+
+        // Disposes of the used shape
+        shape.dispose();
+        return body;
     }
 
     public int getType() {
